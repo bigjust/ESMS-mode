2049a6d3b3e09fabaac9612f82dd64ca73ce5e23
Justin Caratzas <justin.caratzas@cmgdigital.com>
2011-05-07 23:01:37 -0400

    much work for more sane behavior

--- esms.el
+++ esms.el
@@ -1,5 +1,10 @@
-(defvar roster-file "~/SSL/roster/ccy.txt")
-(defvar teamsheet-file "~/SSL/teamsheets/ccysht.txt")
+(define-derived-mode esms-mode nil "ESMS"
+  "Major mode for setting up a ESMS lineup")
+
+(defvar roster-file nil)
+(defvar teamsheet-file nil)
+
+(defvar positions '("GK" "DF" "DM" "MF" "AM" "FW"))
 
 ;; TODO check against lineup
 (defun check-players ()
@@ -7,34 +12,32 @@
   (find-file roster-file)
   (get-unavailable-players))
 
-(defmacro traverse-file (filename start at start-line &rest body)
+(defmacro traverse-roster (filename start at start-line &rest body)
   `(let ((moreLines t))
-    (find-file ,filename)
-    (goto-line ,start-line)
-    (let (( moreLines t))
-      (while moreLines
-	(setq current-line (buffer-substring-no-properties (line-beginning-position) (line-end-position)))
-	(setq current-line (split-string current-line))
-	,@body
-	(setq moreLines (= 0 (forward-line 1)))))))
+     (with-temp-buffer
+       (goto-line ,start-line)
+       (insert-file-contents ,filename)
+       (let (( moreLines t))
+	 (while moreLines
+	   (setq current-line (split-string (buffer-substring-no-properties (line-beginning-position) (line-end-position))))
+	   ,@body
+	   (setq moreLines (= 0 (forward-line 1))))))))
 
 (defun get-unavailable-players ()
+
   (setq unavailable-players nil)
   (let ((unavailable-players nil))
     (traverse-roster roster-file start at 3
-		     (if (> (length current-line) 0)
+		     (if (> (length current-line) 24)
 			 (if (or (> (string-to-number (nth 24 current-line)) 0)
 				 (> (string-to-number (nth 25 current-line)) 0))
-			     (message "%s" (nth 0 current-line))
-			   (append unavailable-players (list (nth 0 current-line))))))
-    (message "%s" (combine-and-quote-strings unavailable-players))))
+			     (setq unavailable-players (cons (nth 0 current-line) unavailable-players)))))
+    unavailable-players))
 
 (defun get-lineup-players ()
   (interactive)
-  (let ((lineup-players nil)
-	(nil-count 0))
+  (let ((players nil))
     (traverse-roster teamsheet-file start at 4
-		     (if (and (< nil-count 2) (> (length current-line) 1))
-			 (message "%s" (nth 1 current-line))
-		       (setq nil-count (+ nil-count 1))))
-    (message "%s" (combine-and-quote-strings lineup-players))))
+		     (if (member (nth 0 current-line) positions)
+			 (setq players (cons (nth 1 current-line) players))))
+    players))
